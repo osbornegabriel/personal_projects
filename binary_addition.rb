17c5fca076656aa98d2574b3c81cd260a4b07c14
Gabriel Osborne <osbornegabriel@gmail.com>
2017-06-04 01:26:39 -0500

    Write much drier version of add_binary method

--- binary_addition.rb
+++ binary_addition.rb
@@ -4,27 +4,34 @@
 
     The binary number returned should be a string.
 
-  Below was my original solution
+  Below was my original solution, after that is my much drier solution, using the .to_i and .to_s(2) methods.
 =end
 
+#Draft 1:
 
+# def add_binary(a,b)
+#   binary_number = '0'
+
+#   (a + b).times do
+#     if !binary_number.include?('0')
+#       binary_number.gsub!('1', '0')
+#       binary_number.prepend('1')
+#     else
+#       change_index = binary_number.rindex('0')
+#       binary_number[change_index] = '1'
+#       change_index += 1
+#       until change_index == binary_number.length
+#         binary_number[change_index] = '0'
+#         change_index += 1
+#       end
+#     end
+#   end
+
+#   binary_number
+# end
+
+#Final Version:
 def add_binary(a,b)
-  binary_number = '0'
-
-  (a + b).times do
-    if !binary_number.include?('0')
-      binary_number.gsub!('1', '0')
-      binary_number.prepend('1')
-    else
-      change_index = binary_number.rindex('0')
-      binary_number[change_index] = '1'
-      change_index += 1
-      until change_index == binary_number.length
-        binary_number[change_index] = '0'
-        change_index += 1
-      end
-    end
-  end
-
-  binary_number
+  sum = (a+b).to_i
+  sum.to_s(2)
 end
